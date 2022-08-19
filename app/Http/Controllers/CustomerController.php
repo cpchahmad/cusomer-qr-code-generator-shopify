15d4560e0743e02d7ffb18f7ae7b06fda7834ced
abdullahjutt40 <abdullah.jatt@tetralogicx.com>
2022-08-19 17:20:04 +0500

    aaa

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -96,7 +96,7 @@ class CustomerController extends Controller
     public function checkStatus($id)
     {
         $data = Customer::where('shopify_customer_id', $id)->first();
-        $html = view('status.view')->with($data)->render();
+        $html = view('status_view')->with($data)->render();
         return response($html)->withHeaders(['Content-Type' => 'application/liquid']);
     }
 }
